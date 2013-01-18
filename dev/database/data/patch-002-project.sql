0ea8f650f23ce8a408e6ef06500a13ba2f6660a8
Nangko <nangko@gmail.com>
2013-01-18 18:33:52 +0100

    removed backticks

--- dev/database/data/patch-002-project.sql
+++ dev/database/data/patch-002-project.sql
@@ -1,5 +1,5 @@
-INSERT INTO `project`
-(`id`,`name`,`description`,`url`)
+INSERT INTO project
+(id,name,description,url)
 values
 (1,'Project A','Sample Project A','http://www.projecta.com'),
 (2,'Project B','Sample Project B','http://www.projectb.com');
--- dev/database/schema/patch-001-project.sql
+++ dev/database/schema/patch-001-project.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS `project` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `description` text COLLATE utf8_unicode_ci NOT NULL,
-  `url` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS project (
+  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  description text COLLATE utf8_unicode_ci NOT NULL,
+  url varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
